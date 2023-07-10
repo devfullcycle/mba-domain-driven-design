eacbb11a7d77246a5267b5dd052970b2161ade97
Luiz Carlos <argentinaluiz@gmail.com>
2023-07-09 23:21:27 -0300

    Adicionando contrato para os repositórios

---
+++ src/@core/common/domain/repository-interface.ts
@@ -0,0 +1,8 @@
+import { AggregateRoot } from './aggregate-root';
+
+export interface IRepository<E extends AggregateRoot> {
+  add(entity: E): Promise<void>;
+  findById(id: any): Promise<E | null>;
+  findAll(): Promise<E[]>;
+  delete(id: any): Promise<void>;
+}
---
+++ src/@core/events/domain/repositories/partner.repository.interface.ts
@@ -0,0 +1,5 @@
+import { IRepository } from '../../../common/domain/repository-interface';
+import { Partner } from '../entities/partner.entity';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface PartnerRepositoryInterface extends IRepository<Partner> {}

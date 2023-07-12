14979609e294ca8b953eef19e94f8cd0063acc6a
Luiz Carlos <argentinaluiz@gmail.com>
2023-07-12 16:57:18 -0300

    Sobre tamanho dos application services

--- apps/mba-ddd-venda-ingresso/src/events/events/events.controller.ts
+++ apps/mba-ddd-venda-ingresso/src/events/events/events.controller.ts
@@ -8,7 +8,9 @@ export class EventsController {
 
   @Get()
   async list() {
-    return this.eventService.findEvents();
+    const events = await this.eventService.findEvents();
+    logSizeInBytes('events', events[0]);
+    return events;
   }
 
   @Post()
@@ -24,3 +26,22 @@ export class EventsController {
     return this.eventService.publishAll({ event_id: event_id });
   }
 }
+
+const getSizeInBytes = (obj) => {
+  let str = null;
+  if (typeof obj === 'string') {
+    // If obj is a string, then use it
+    str = obj;
+  } else {
+    // Else, make obj into a string
+    str = JSON.stringify(obj);
+  }
+  // Get the length of the Uint8Array
+  const bytes = new TextEncoder().encode(str).length;
+  return bytes;
+};
+
+const logSizeInBytes = (description, obj) => {
+  const bytes = getSizeInBytes(obj);
+  console.log(`${description} is approximately ${bytes} B`);
+};

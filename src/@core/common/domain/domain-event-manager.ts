cab84572db60c4ae5dc3e3152a80bf8cc793e45e
Luiz Carlos <argentinaluiz@gmail.com>
2023-07-12 11:56:09 -0300

    Criando gerenciador de eventos

---
+++ src/@core/common/domain/domain-event-manager.ts
@@ -0,0 +1,23 @@
+import EventEmitter2 from 'eventemitter2';
+import { AggregateRoot } from './aggregate-root';
+
+export class DomainEventManager {
+  eventEmitter: EventEmitter2;
+
+  constructor() {
+    this.eventEmitter = new EventEmitter2({
+      wildcard: true,
+    });
+  }
+
+  register(event: string, handler: any) {
+    this.eventEmitter.on(event, handler);
+  }
+
+  async publish(aggregateRoot: AggregateRoot) {
+    for (const event of aggregateRoot.events) {
+      const eventClassName = event.constructor.name;
+      await this.eventEmitter.emitAsync(eventClassName, event);
+    }
+  }
+}
